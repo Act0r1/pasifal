15eb88006c4f0a732b344d8b93c4a952a19c57f8
0xAct0r <gizatullininsaf25@gmail.com>
2023-03-10 12:03:11 +0300

    impl new for game struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, prelude::*};
 
-const GREETING:&str = "Welcome in Pasifal, it is tiny\
+const GREETING: &str = "Welcome in Pasifal, it is tiny\
                        chess engine in Rust. Press Enter for\
                        starting game, if you want to choose you side\
                        just type it here, by default you will play\
@@ -15,11 +15,7 @@ fn main() {
         let mut input = String::new();
         println!("{:?}", GREETING);
         let stdin = io::stdin();
-
         stdin.lock().read_line(&mut input).unwrap();
-
         println!("You typed: {}", input);
-        
     }
-    
 }
--- src/moving.rs
+++ src/moving.rs
@@ -1,17 +1,56 @@
-use crate::board::{Board};
+use std::collections::HashSet;
+
+use crate::board::Board;
 
 #[derive(Debug)]
 struct Move {
     from: (usize, usize),
     to: (usize, usize),
-    promotion: bool
+    promotion: bool,
+}
+
+enum StatusGame {
+    Playing,
+    End,
+}
+
+struct Game {
+    status: StatusGame,
+    white_pieces: HashSet<(i32, String)>,
+    black_pieces: HashSet<(i32, String)>,
+    board: Board,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            status: StatusGame::Playing,
+            white_pieces: HashSet::from([
+                (2, "Rook".to_string()),
+                (8, "Pawn".to_string()),
+                (2, "Bishop".to_string()),
+                (2, "Knight".to_string()),
+                (1, "Queen".to_string()),
+                (1, "King".to_string()),
+            ]),
+            black_pieces: HashSet::from([
+                (2, "Rook".to_string()),
+                (8, "Pawn".to_string()),
+                (2, "Bishop".to_string()),
+                (2, "Knight".to_string()),
+                (1, "Queen".to_string()),
+                (1, "King".to_string()),
+            ]),
+            board: Board::new()
+        }
+    }
 }
 
 enum Moves {
-  Illegal,
-  Legal
+    Illegal,
+    Legal,
 }
 
-pub fn make_move(board:&Board) {
-  todo!()
+pub fn make_move(board: &Board) {
+    todo!()
 }
